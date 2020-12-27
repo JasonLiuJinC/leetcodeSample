a5acac71e301bf11b0a797daebde9985d0c85341
liujinchang <2746028@qq.com>
2020-12-27 17:02:15 +0800

    feat 9 回文数，14 最长公共前缀，13 罗马数字转整数

---
+++ src/com/leetcode/simple/回文数9.java
@@ -0,0 +1,42 @@
+package com.leetcode.simple;
+
+/**
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ * 示例 1:
+ *
+ * 输入: 121
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 示例 3:
+ *
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ * 进阶:
+ *
+ * 你能不将整数转为字符串来解决这个问题吗？
+ *
+ */
+public class 回文数9 {
+    public static void main(String[] args) {
+        boolean palindrome = isPalindrome(10);
+        System.out.println("palindrome = " + palindrome);
+    }
+
+    public static boolean isPalindrome(int x) {
+       if(x<0)return false;
+       int a=0;
+       int temp=x;
+       while(temp>=1){
+           a=a*10+temp%10;
+           temp=temp/10;
+       }
+       if(x!=a)return false;
+       return true;
+    }
+}
